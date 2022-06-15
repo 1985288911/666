369b7d1dc1f18d6586f1b97497404cbe306f8971
linweiyu <1985288911@qq.com>
2022-06-15 09:42:41 +0800

    mixin:混入对象\modules:菜单\view:第一子组件main

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<!-- 目前的首页 打包之后首页是index.html -->
+<!-- 
+  定义:目前的页面入口文件 打包之后首页是index.html
+  
+  主要有三个作用：
+  1.主组件。
+  2.放置项目中所有页面公共需要的动画或者样式。
+  3.所有的页面都是在app.vue中进行切换的。可以理解为所有的路由都是app.vue的子组件。
+ -->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+<!-- 
+  定义:main.js是项目的入口文件，项目中所有的页面都会加载main.js。
+
+  主要有三个作用：
+  1.实例化Vue。
+  2.放置项目中经常会用到的插件和CSS样式。
+  3.存储全局变量。
+ -->
 import Vue from 'vue'
 import App from './App.vue'
 
